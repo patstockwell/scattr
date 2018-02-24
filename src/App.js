5b573e1956a0952050fdd7ef219297bf6eb601f5
Patrick Stockwell <stockwellpatrick@gmail.com>
2018-02-25 00:06:59 +1100

    add new comment functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 // import { Helmet } from 'react-helmet'
+import profileImage from './assets/profile-picture.png'
 import PostFeed from './components/PostFeed'
 import samplePosts from './samplePosts'
 
@@ -7,14 +8,45 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      newCommentValue: '',
       samplePosts,
     }
+    this.addComment = this.addComment.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  addComment(event) {
+    event.preventDefault()
+    const newComment = {
+      id: 234234285,
+      firstName: 'Patrick',
+      lastName: 'Stockwell',
+      profileImage,
+      postContent: this.state.newCommentValue,
+      claps: 0,
+      comments: [],
+    }
+
+    this.setState(prevState => ({
+      samplePosts: [newComment, ...prevState.samplePosts]
+    }))
+  }
+
+  handleInput(event) {
+    event.preventDefault()
+    this.setState({
+      newCommentValue: event.target.value,
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h2>Scattr</h2>
+        <form onSubmit={this.addComment} >
+          <input type="text" value={this.state.newCommentValue} onChange={this.handleInput} />
+          <input type="submit" value="Post" />
+        </form>
         <PostFeed posts={this.state.samplePosts} />
       </div>
     )
--- src/samplePosts.js
+++ src/samplePosts.js
@@ -1,9 +1,14 @@
 import profileImage from './assets/profile-picture.png'
 
-const sampleComments = [
+const sampleComments1 = [
   { name: 'Zosia', content: 'sounds like a good idea', id: 98273 },
   { name: 'Zosia', content: 'When can we start?', id: 98274 },
   { name: 'Patrick', content: 'How about next Tuesday?', id: 98275 },
+]
+
+const sampleComments2 = [
+  { name: 'James', content: 'Schweeet', id: 98274 },
+  { name: 'Frederick', content: 'lol', id: 98275 },
 ]
 
 const samplePosts = [
@@ -14,8 +19,26 @@ const samplePosts = [
     profileImage,
     postContent: 'The is the first idea to be posted to Scattr',
     claps: 25,
-    comments: sampleComments,
-  }
+    comments: sampleComments1,
+  },
+  {
+    id: 234234275,
+    firstName: 'Patrick',
+    lastName: 'Stockwell',
+    profileImage,
+    postContent: 'The is the second idea to be posted to Scattr. It\'s a bit longer than the first. It also has a different key.',
+    claps: 8,
+    comments: sampleComments2,
+  },
+  {
+    id: 234234276,
+    firstName: 'Patrick',
+    lastName: 'Stockwell',
+    profileImage,
+    postContent: 'Remove handles from the "push-side" of the door',
+    claps: 41,
+    comments: sampleComments2,
+  },
 ]
 
 export default samplePosts
