05ecb87b202f07d971afbaee5d33a9aebb16ad85
Patrick Stockwell <stockwellpatrick@gmail.com>
2018-02-25 23:02:25 +1100

    add columns to layout file

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import Clap from './Clap'
 import SpeechBubble from './SpeechBubble'
 import Comments from './Comments'
+import { TenColumns } from '../utilities/layout'
+
+const ProfileImage = styled.img`
+  max-width: 50px;
+  max-height: 50px;
+  border-radius: 50%;
+`
+
+const NameAndContent = styled.div`
+  display: inline-block;
+  width: ${TenColumns}%;
+`
 
 const Post = props => (
   <div>
-    <img className="post-profile-picture" src={props.profileImage} alt={`${props.firstName} ${props.lastName}`} />
-    <p className="post-full-name">{`${props.firstName} ${props.lastName}`}</p>
-    <p className="post-content">{props.postContent}</p>
+    <ProfileImage className="post-profile-picture" src={props.profileImage} alt={`${props.firstName} ${props.lastName}`} />
+    <NameAndContent>
+      <p className="post-full-name">{`${props.firstName} ${props.lastName}`}</p>
+      <p className="post-content">{props.postContent}</p>
+    </NameAndContent>
     <Clap />
     <span>{props.claps}</span>
     <SpeechBubble />
---
+++ src/utilities/layout.js
@@ -0,0 +1,11 @@
+export const OneColumn = 4.66666666667
+export const TwoColumns = 13.3333333333
+export const ThreeColumns = 22
+export const FourColumns = 30.6666666667
+export const FiveColumns = 39.3333333333
+export const SixColumns = 48
+export const SevenColumns = 56.6666666667
+export const EightColumns = 65.3333333333
+export const NineColumns = 74
+export const TenColumns = 82.6666666667
+export const ElevenColumns = 91.3333333333
