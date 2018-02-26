30595be78d9de052afc5b3ea96920835c47d529a
Patrick Stockwell <stockwellpatrick@gmail.com>
2018-02-26 23:23:08 +1100

    Add columns to feed layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,26 +4,42 @@ import styled from 'styled-components'
 import Clap from './Clap'
 import SpeechBubble from './SpeechBubble'
 import Comments from './Comments'
-import { TenColumns } from '../utilities/layout'
+import { TenColumns, TwoColumns, ColumnLeftMargin } from '../utilities/layout'
 
 const ProfileImage = styled.img`
   max-width: 50px;
   max-height: 50px;
   border-radius: 50%;
+  width: 100%;
 `
 
 const NameAndContent = styled.div`
   display: inline-block;
-  width: ${TenColumns}%;
+  width: ${TenColumns};
+  margin-left: ${ColumnLeftMargin};
+`
+
+const ImageColumn = styled.div`
+  display:inline-block;
+  width: ${TwoColumns};
+`
+
+const FlexDiv = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 const Post = props => (
   <div>
-    <ProfileImage className="post-profile-picture" src={props.profileImage} alt={`${props.firstName} ${props.lastName}`} />
-    <NameAndContent>
-      <p className="post-full-name">{`${props.firstName} ${props.lastName}`}</p>
-      <p className="post-content">{props.postContent}</p>
-    </NameAndContent>
+    <FlexDiv>
+      <ImageColumn>
+        <ProfileImage className="post-profile-picture" src={props.profileImage} alt={`${props.firstName} ${props.lastName}`} />
+      </ImageColumn>
+      <NameAndContent>
+        <p className="post-full-name">{`${props.firstName} ${props.lastName}`}</p>
+        <p className="post-content">{props.postContent}</p>
+      </NameAndContent>
+    </FlexDiv>
     <Clap />
     <span>{props.claps}</span>
     <SpeechBubble />
